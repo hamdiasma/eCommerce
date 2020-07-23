01ddc26b0722c5b7c6b4ddc914b6db9e4728ee8d
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-23 14:53:41 +0100

    scss home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,34 @@
 import React from "react";
-
+import "./homePage.scss";
 const HomePage = () => {
   return (
-    <div className="home-page">
+    <div className="homepage">
       <div className="directory-menu">
-        <div className="menue-item">
+        <div className="menu-item">
           <div className="content">
             <h1 className="title">HATS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
-        <div className="menue-item">
+        <div className="menu-item">
           <div className="content">
             <h1 className="title">JACKETS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
-        <div className="menue-item">
+        <div className="menu-item">
           <div className="content">
             <h1 className="title">SNEAKERS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
-        <div className="menue-item">
+        <div className="menu-item">
           <div className="content">
             <h1 className="title">WOMENS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
-        <div className="menue-item">
+        <div className="menu-item">
           <div className="content">
             <h1 className="title">MENS</h1>
             <span className="subtitle">SHOP NOW</span>
