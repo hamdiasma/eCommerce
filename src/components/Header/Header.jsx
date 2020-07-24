ba45362bd7661dba2126f7b7880ad4a6eaedcdfb
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-24 15:14:47 +0100

    integreate firebase created sininpage and su=inin with google

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/ShopLogo.png";
-import "./Header.scss"
+import { auth } from "../../firebase/firebase";
+import "./Header.scss";
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
-        <img className="logo" src={Logo} alt="" /> <strong> My E-COMMERCE</strong>
+        <img className="logo" src={Logo} alt="" />{" "}
+        <strong> My E-COMMERCE</strong>
       </Link>
       <div className="options">
-        <Link className="option" to="/">HOME</Link>
-        <Link className="option" to="/shop">SHOP</Link>
-        <Link className="option" to="/contact">CONTACT</Link>
+        <Link className="option" to="/">
+          HOME
+        </Link>
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SignOut <img className="user-img" src={currentUser.photoURL} alt=""/>
+          </div>
+        ) : (
+          <Link className="option" to="signin">
+            SignIn
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -52,8 +52,12 @@ class SignIn extends Component {
             label="Password"
             required
           />
-          <CustomButton type="submit">SIGN IN</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">SIGN IN</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              SIGN IN WITH GOOGLE
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
