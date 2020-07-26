64334fefffdf6677d7fec9a4fcd33ddd808aa82f
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-26 18:11:57 +0100

    install redux react-redux redux-logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import { Provider } from "react-redux";
+
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
