dab4a2e7cf4957be770a31db26b0994574016fcf
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-28 12:59:53 +0100

    updating app with reselect replase cart selectors memorise cartSelectors

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./CartIcon.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
-import { toggleCartHidden } from "../../redux/cart/cartAction";
 
-const CartIcon = ({ toggleCartHidden ,itemCount}) => {
+import { toggleCartHidden } from "../../redux/cart/cartAction";
+import { selectCartCountItem } from "../../redux/cart/cart.selectors";
+
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
@@ -13,12 +15,9 @@ const CartIcon = ({ toggleCartHidden ,itemCount}) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
+const mapStateToProps = (state) => {
   return {
-    itemCount: cartItems.reduce(
-      (accumulateQuantity, cartItem) => accumulateQuantity + cartItem.quantity,
-      0
-    ),
+    itemCount: selectCartCountItem(state),
   };
 };
 
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,15 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartCountItem = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumulateQuantity, cartItem) => accumulateQuantity + cartItem.quantity,
+    0
+  )
+);
